08d6e0d1f3ae197b5e6c60d9be9f1a8f21e56de2
ditara <dhitta28.dr@gmail.com>
2020-05-15 21:20:03 +0700

    update detail

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -14,6 +14,28 @@
               class="btn btn-danger mt-4">Add To Favourites</button>
             </div>
             <p class="text-justify mt-4">
+{{data.strIngredient1}} <span class="badge badge-pill badge-warning">{{data.strMeasure1}}</span>,
+{{data.strIngredient2}} <span class="badge badge-pill badge-warning">{{data.strMeasure2}}</span>,
+{{data.strIngredient3}} <span class="badge badge-pill badge-warning">{{data.strMeasure3}}</span>,
+{{data.strIngredient4}} <span class="badge badge-pill badge-warning">{{data.strMeasure4}}</span>,
+{{data.strIngredient5}} <span class="badge badge-pill badge-warning">{{data.strMeasure5}}</span>,
+{{data.strIngredient6}} <span class="badge badge-pill badge-warning">{{data.strMeasure6}}</span>,
+{{data.strIngredient7}} <span class="badge badge-pill badge-warning">{{data.strMeasure7}}</span>,
+{{data.strIngredient8}} <span class="badge badge-pill badge-warning">{{data.strMeasure8}}</span>,
+{{data.strIngredient9}} <span class="badge badge-pill badge-warning">{{data.strMeasure9}}</span>,
+{{data.strIngredient10}} <span class="badge badge-pill badge-warning">{{data.strMeasure10}}</span>,
+{{data.strIngredient11}} <span class="badge badge-pill badge-warning">{{data.strMeasure11}}</span>,
+{{data.strIngredient12}} <span class="badge badge-pill badge-warning">{{data.strMeasure12}}</span>,
+{{data.strIngredient13}} <span class="badge badge-pill badge-warning">{{data.strMeasure13}}</span>,
+{{data.strIngredient14}} <span class="badge badge-pill badge-warning">{{data.strMeasure14}}</span>,
+{{data.strIngredient15}} <span class="badge badge-pill badge-warning">{{data.strMeasure15}}</span>,
+{{data.strIngredient16}} <span class="badge badge-pill badge-warning">{{data.strMeasure16}}</span>,
+{{data.strIngredient17}} <span class="badge badge-pill badge-warning">{{data.strMeasure17}}</span>,
+{{data.strIngredient18}} <span class="badge badge-pill badge-warning">{{data.strMeasure18}}</span>,
+{{data.strIngredient19}} <span class="badge badge-pill badge-warning">{{data.strMeasure19}}</span>,
+{{data.strIngredient20}} <span class="badge badge-pill badge-warning">{{data.strMeasure20}}</span>,
+            </p>
+            <p class="text-justify mt-4">
                 {{ data.strInstructions }}
             </p>
         </div>
